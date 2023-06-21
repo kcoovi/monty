70ab7274bf4fc9f0c6f2ff5074f571c67b01d49d
Kevin COOVI <k.coovi@gmail.com>
2023-06-21 19:31:17 +0300

    C - Stacks, Queues - LIFO, FIFO

--- pall.c
+++ pall.c
@@ -7,6 +7,7 @@
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
+(void)line_number
 stack_t *current = *stack;
 
 while (current != NULL)
--- push.c
+++ push.c
@@ -1,6 +1,7 @@
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * push - push
  * @stack: stack
